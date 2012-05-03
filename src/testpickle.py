aebb7523f3ae835ee8bd95e55b31b03fa3344ccf
chiz <chzhcn88@gmail.com>
2012-05-03 16:13:51 -0400

    test pickle

---
+++ src/testpickle.py
@@ -0,0 +1,16 @@
+import cpickle
+import myclass
+import socket
+
+
+if __name__ == '__main__' :
+    obj = myclass()
+
+    with open('pfile', 'wb') as f:
+        cpickle.dump(obj, f)
+
+    address = ('128.237.120.95', 30001)
+    s = socket.create_connection(address)
+
+    with open('pfile', 'rb') as f :
+        s.sendall(f.read())
